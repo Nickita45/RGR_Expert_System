2404d1d95d5494fbe34c727fe788f3474112c8ad
Nickita45 <supernikita45@gmail.com>
2020-12-16 23:22:54 +0200

    first commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppRgr
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public DiagnosticExpertSystem Expert { get; private set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Expert = new DiagnosticExpertSystem();
+        }
+
+        private void loadButton_Click(object sender, RoutedEventArgs e)
+        {
+            //Configure open file dialog box.
+            var dlg = new Microsoft.Win32.OpenFileDialog
+            {
+                Title = "Вибір бази знань",
+                DefaultExt = ".xml",
+                Filter = "XML-файл (.xml)|*.xml",
+                CheckFileExists = true
+            };
+
+            //Process open file dialog.
+            if (dlg.ShowDialog() == true)
+            {
+                try
+                {
+                    Expert.LoadDatabase(dlg.FileName);
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Під час завантаження бази знань сталася помилка.", "Помилка",
+                                    MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+    }
+}
---
+++ NotifiableCollection.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace WpfAppRgr
+{
+    public class NotifiableCollection<T> : ObservableCollection<T>
+        where T : class, INotifyPropertyChanged
+    {
+        /// <summary>
+        /// Occurs when collection item property changes.
+        /// </summary>
+        public event EventHandler<ItemChangedEventArgs> ItemChanged;
+
+        /// <summary>
+        /// Invokes the ItemChanged event.
+        /// </summary>
+        /// <param name="e">The ItemChangedEventArgs instance containing the item index.</param>
+        public void InvokeItemChanged(ItemChangedEventArgs e)
+        {
+            if (ItemChanged != null)
+            {
+                ItemChanged(this, e);
+            }
+        }
+
+        /// <summary>
+        /// Clears the items.
+        /// </summary>
+        protected override void ClearItems()
+        {
+            foreach (var item in Items)
+            {
+                item.PropertyChanged -= ItemPropertyChanged;
+            }
+            base.ClearItems();
+        }
+
+        /// <summary>
+        /// Sets the item.
+        /// </summary>
+        /// <param name="index">The index.</param>
+        /// <param name="item">The item.</param>
+        protected override void SetItem(int index, T item)
+        {
+            Items[index].PropertyChanged -= ItemPropertyChanged;
+            base.SetItem(index, item);
+            item.PropertyChanged += ItemPropertyChanged;
+        }
+
+        /// <summary>
+        /// Removes the item.
+        /// </summary>
+        /// <param name="index">The index.</param>
+        protected override void RemoveItem(int index)
+        {
+            Items[index].PropertyChanged -= ItemPropertyChanged;
+            base.RemoveItem(index);
+        }
+
+        /// <summary>
+        /// Inserts the item.
+        /// </summary>
+        /// <param name="index">The index.</param>
+        /// <param name="item">The item.</param>
+        protected override void InsertItem(int index, T item)
+        {
+            base.InsertItem(index, item);
+            item.PropertyChanged += ItemPropertyChanged;
+        }
+
+        /// <summary>
+        /// Items property changed event handler.
+        /// </summary>
+        /// <param name="sender">The item.</param>
+        /// <param name="e">The PropertyChangedEventArgs instance containing the item property name.</param>
+        private void ItemPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            var item = sender as T;
+            InvokeItemChanged(new ItemChangedEventArgs(Items.IndexOf(item), e.PropertyName));
+        }
+    }
+
+    /// <summary>
+    /// Provides data for ItemChanged event.
+    /// </summary>
+    public class ItemChangedEventArgs : PropertyChangedEventArgs
+    {
+        /// <summary>
+        /// Index of item that was changed.
+        /// </summary>
+        public int Index { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the ItemChangedEventArgs class.
+        /// </summary>
+        /// <param name="index">Index of changed item.</param>
+        /// <param name="propertyName">Name of changed property in item.</param>
+        public ItemChangedEventArgs(int index, string propertyName)
+            : base(propertyName)
+        {
+            Index = index;
+        }
+    }
+}
